50c5db380124217ca50afc6bd22545074129945d
ᴀꜱʜɪᴋ ᴍʜᴅ <85797232+Ashik231@users.noreply.github.com>
2022-07-11 13:59:32 +0530

    Update script.py

--- script.py
+++ script.py
@@ -12,7 +12,7 @@ class Script(object):
 <b>Here Is The Help For My Commands.</b>"""
 
     ABOUT_TXT = """<b>➥ My name: {}
-➥ 𝘔𝘢𝘴𝘵𝘦𝘳 𝘉𝘳𝘢𝘪𝘯: <a href='https://t.me/Unavailable4allTime'>𝘈𝘴𝘩𝘪𝘬 𝘔𝘩𝘥</a>"""
+➥ 𝘔𝘢𝘴𝘵𝘦𝘳 𝘉𝘳𝘢𝘪𝘯: 𝘈𝘴𝘩𝘪𝘬 𝘔𝘩𝘥
 ➥ Library: pyrogram
 ➥ 𝘓𝘢𝘯𝘨𝘶𝘢𝘨𝘦: 𝘗𝘺𝘵𝘩𝘰𝘯 𝟹
 ➥ 𝘋𝘢𝘵𝘢 𝘉𝘢𝘴𝘦: 𝘔𝘢𝘯𝘨𝘰𝘋𝘉
